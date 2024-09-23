0db4f6d7dee497eb7892078779968ee753d9e4df
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2024-09-23 21:36:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Four Elements/four-elements.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.util.Arrays;
+import java.util.Scanner;
+class FindFourElements
+{
+	
+    public static void main(String[] args) 
+    {
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			FindFourElements findfour = new FindFourElements();
+			int n=sc.nextInt();
+			int[] A=new int[n];
+			for(int i=0;i<n;i++)
+				A[i]=sc.nextInt();
+			int X =sc.nextInt();
+			
+			Compute ob = new Compute();
+		    System.out.println(ob.find4Numbers(A, n, X)?1:0);
+		}
+    }
+}
+
+
+// } Driver Code Ends
+
+
+class Compute
+{
+    boolean find4Numbers(int arr[], int n, int x) 
+    {
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                for(int k=j+1; k<n; k++){
+                    for(int l=k+1; l<n; l++){
+                        if(arr[i]+arr[j]+arr[k]+arr[l]==x) return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
