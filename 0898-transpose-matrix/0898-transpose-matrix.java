8f6120bf9f33022f87633d0d68bcbaac4a1a1b4a
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2024-12-24 22:15:02 +0530

    Time: 1 ms (28.63%), Space: 45.4 MB (7.15%) - LeetHub

---
+++ 0898-transpose-matrix/0898-transpose-matrix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int r=matrix.length, c=matrix[0].length;
+        int[][] arr = new int[c][r];
+        for (int i=0; i<matrix.length; i++){
+            for (int j=0; j<matrix[i].length; j++){
+                arr[j][i]=matrix[i][j];
+            }
+        }
+        return arr;
+    }
+}
