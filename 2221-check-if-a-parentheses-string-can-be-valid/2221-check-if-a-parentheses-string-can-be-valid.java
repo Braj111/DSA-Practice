5c15775bd6fe3a9d5774e739da703b0e9da97aab
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-01-12 23:09:03 +0530

    Time: 16 ms (58.92%), Space: 46 MB (28.63%) - LeetHub

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        int n=s.length();
+        if(n%2==1) return false;
+        
+        int balance = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='(' || locked.charAt(i)=='0') balance++;
+            else if(balance >0) balance --;
+            else return false;
+        }
+
+        balance = 0;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i)==')' || locked.charAt(i)=='0') balance++;
+            else if(balance >0) balance --;
+            else return false;
+        }
+        return true;
+    }
+}
