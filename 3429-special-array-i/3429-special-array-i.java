3cfd31d8391743bc29b80516a0f426cad6de3ccb
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-02-20 12:16:13 +0530

    Time: 0 ms (100%), Space: 43.5 MB (32.45%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        for (int i = 1; i < nums.length; ++i) {
-            if (nums[i] % 2 == nums[i - 1] % 2) {
-                return false;
-            }
+        for(int i=1; i<nums.length; i++){
+            if((nums[i]+nums[i-1])%2==0) return false;
         }
         return true;
     }
