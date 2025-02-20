982228693239da79cf32cae09c42af3008250aff
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-02-20 18:27:32 +0530

    Time: 0 ms (100%), Space: 41.9 MB (13.78%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-        int res = 0; 
-        char firstCharFromS1 = 0, firstCharFromS2 = 0; 
-        for (int i = 0; i < s1.length(); ++i) {
-            char charFromS1 = s1.charAt(i), charFromS2 = s2.charAt(i); 
-            if (charFromS1 != charFromS2) {
-                if (++res > 2 || 
-                    (res == 2 && !(charFromS1 == firstCharFromS2 && charFromS2 == firstCharFromS1))) {
-                    return false;
-                }
-                firstCharFromS1 = charFromS1;
-                firstCharFromS2 = charFromS2;
+       if(s1==s2) return true;
+       char[] arr= new char[4];
+       int j=0;
+       for(int i=0; i<s1.length(); i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                if(j>2) return false; 
+                arr[j]=s1.charAt(i); j++;
+                arr[j]=s2.charAt(i); j++;
             }
-        }
-        return res != 1;
+       }
+       if(arr[0]==arr[3] && arr[1]==arr[2]) return true;   
+       return false;
     }
 }
