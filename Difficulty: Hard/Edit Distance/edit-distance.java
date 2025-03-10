359edab3610d207737bbc272421a92f4ba829ad2
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-03-10 23:22:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.java
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -9,12 +9,13 @@ class GFG {
         int T = Integer.parseInt(br.readLine().trim());
         while (T-- > 0) {
             String s1 = br.readLine();
-            String[] S = s1.split(" ");
-            String s = S[0];
-            String t = S[1];
+            String s2 = br.readLine();
+
             Solution ob = new Solution();
-            int ans = ob.editDistance(s, t);
+            int ans = ob.editDistance(s1, s2);
             System.out.println(ans);
+
+            System.out.println("~");
         }
     }
 }
@@ -22,31 +23,31 @@ class GFG {
 // } Driver Code Ends
 
 
-
-
 class Solution {
-    public int editDistance(String str1, String str2) {
+    public int editDistance(String s1, String s2) {
         // Code here
-        int m= str1.length();
-        int n= str2.length();
-        return sol(str1,str2,m,n);
-    }
-    int sol(String s1, String s2, int m, int n){
-        int dp[][] = new int[m+1][n+1];
-        //Base Case
-        for(int i=0; i<=n; i++){dp[0][i]=i;}
-        for(int i=0; i<=m; i++){dp[i][0]=i;}
+        int m=s1.length();
+        int n=s2.length();
         
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1))
-                    dp[i][j]=dp[i-1][j-1];
-                else{
-                    dp[i][j]=1+Math.min(dp[i-1][j-1], 
-                    Math.min(dp[i][j-1], dp[i-1][j]));
-                }
+        int prev;
+        int[] curr=new int[n+1];
+        
+        for (int j = 0; j <= n; j++) 
+            curr[j] = j;
+
+        for (int i = 1; i <= m; i++) {
+            prev = curr[0];
+            curr[0] = i;
+            for (int j = 1; j <= n; j++) {
+                int temp = curr[j];
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    curr[j] = prev;
+                else
+                    curr[j] = 1 + Math.min(Math.min(curr[j - 1], prev), curr[j]);
+                prev = temp;
             }
         }
-        return dp[m][n];
+
+        return curr[n];
     }
 }
