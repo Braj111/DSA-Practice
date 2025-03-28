6ce06b27e90ff6ce60662af4f96ca068f4fe0d02
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-03-28 22:38:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Activity Selection/activity-selection.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(reader.readLine().trim());
+
+        while (t-- > 0) {
+            // Read the start times
+            String[] startInput = reader.readLine().trim().split("\\s+");
+            int[] start = new int[startInput.length];
+            for (int i = 0; i < startInput.length; i++) {
+                start[i] = Integer.parseInt(startInput[i]);
+            }
+
+            // Read the end times
+            String[] endInput = reader.readLine().trim().split("\\s+");
+            int[] finish = new int[endInput.length];
+            for (int i = 0; i < endInput.length; i++) {
+                finish[i] = Integer.parseInt(endInput[i]);
+            }
+
+            // Create solution object and call activitySelection
+            Solution obj = new Solution();
+            System.out.println(obj.activitySelection(start, finish));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public int activitySelection(int[] start, int[] finish) {
+        // code here.
+        int n = start.length;
+        int ans = 0;
+        PriorityQueue<int[]> p = new PriorityQueue<>(
+            (a, b) -> Integer.compare(a[0], b[0]));
+
+        for (int i = 0; i < n; i++) {
+            p.add(new int[] { finish[i], start[i] });
+        }
+        int finishtime = -1;
+
+        while (!p.isEmpty()) {
+            int[] activity= p.poll();
+                           
+            if (activity[1] > finishtime) {
+                finishtime = activity[0];
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
