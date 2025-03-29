f085cc13a76d843654b22e754166df7582564e27
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-03-29 13:23:35 +0530

    Time: 92 ms (73.68%), Space: 56.1 MB (87.22%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.java
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.java
@@ -1,53 +1,67 @@
 class Solution {
-  public int[] maxPoints(int[][] grid, int[] queries) {
-    record T(int i, int j, int val) {}
-    final int[][] DIRS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    final int m = grid.length;
-    final int n = grid[0].length;
-    int[] ans = new int[queries.length];
-    Queue<T> minHeap = new PriorityQueue<>(Comparator.comparingInt(T::val));
-    boolean[][] seen = new boolean[m][n];
-
-    minHeap.offer(new T(0, 0, grid[0][0]));
-    seen[0][0] = true;
-    int accumulate = 0;
-
-    for (IndexedQuery indexedQuery : getIndexedQueries(queries)) {
-      final int queryIndex = indexedQuery.queryIndex;
-      final int query = indexedQuery.query;
-      while (!minHeap.isEmpty()) {
-        final int i = minHeap.peek().i;
-        final int j = minHeap.peek().j;
-        final int val = minHeap.poll().val;
-        if (val >= query) {
-          minHeap.offer(new T(i, j, val));
-          break;
+    class Pair{
+        int first;
+        int second;
+        Pair(int first, int second ){
+            this.first=first;
+            this.second=second;
         }
-        ++accumulate;
-        for (int[] dir : DIRS) {
-          final int x = i + dir[0];
-          final int y = j + dir[1];
-          if (x < 0 || x == m || y < 0 || y == n)
-            continue;
-          if (seen[x][y])
-            continue;
-          minHeap.offer(new T(x, y, grid[x][y]));
-          seen[x][y] = true;
-        }
-      }
-      ans[queryIndex] = accumulate;
     }
+    class Tripple{
+        int first;
+        Pair second;
+        Tripple(int first, Pair second ){
+            this.first=first;
+            this.second=second;
+        }
+    }
+    public boolean isValid(int x, int y, int m, int n){
+        return x>=0 && x<m && y>=0 && y<n;
+    }
+    public int[] maxPoints(int[][] grid, int[] queries) {
+        Set<Integer> sortedQueries = new TreeSet<>();
+        for(int q: queries) sortedQueries.add(q);
 
-    return ans;
-  }
+        Map<Integer, Integer> queryCount = new HashMap<>();
+        PriorityQueue<Tripple> minheap = new PriorityQueue<>((a,b)->a.first-b.first);
 
-  private record IndexedQuery(int queryIndex, int query) {}
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int[] dir= {-1,0,1,0,-1};
 
-  private IndexedQuery[] getIndexedQueries(int[] queries) {
-    IndexedQuery[] indexedQueries = new IndexedQuery[queries.length];
-    for (int i = 0; i < queries.length; ++i)
-      indexedQueries[i] = new IndexedQuery(i, queries[i]);
-    Arrays.sort(indexedQueries, Comparator.comparingInt(IndexedQuery::query));
-    return indexedQueries;
-  }
+        //Process 1st grid element
+        minheap.add(new Tripple(grid[0][0],new Pair(0,0)));
+        visited[0][0]=true;
+
+        int count=0;
+        for(int q:sortedQueries){
+            while(!minheap.isEmpty()){
+                Tripple e = minheap.poll();
+                int val= e.first;
+                int x=e.second.first;
+                int y=e.second.second;
+
+                if(val>=q){
+                    minheap.add(e);
+                    break;
+                }
+                count++;
+
+                for(int i=0; i<4; i++){
+                    int newX = x+dir[i];
+                    int newY= y+dir[i+1];
+                    if(isValid(newX,newY,m,n) && !visited[newX][newY]){
+                        visited[newX][newY]=true;
+                        minheap.add(new Tripple(grid[newX][newY], new Pair(newX,newY)));
+                    }
+                }
+            }
+            queryCount.put(q, count);
+        }
+        int[] res= new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            res[i] = queryCount.get(queries[i]);
+        }
+        return res;
+    }
 }
