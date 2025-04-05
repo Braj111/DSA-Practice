69cd2583638a67f9ee4c0599ef91af32235bc5db
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-04-06 00:19:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+
+            int[][] edges = new int[E][2];
+            for (int i = 0; i < E; i++) {
+                edges[i][0] = sc.nextInt();
+                edges[i][1] = sc.nextInt();
+                adj.get(edges[i][0]).add(edges[i][1]);
+            }
+
+            ArrayList<Integer> res = new Solution().topoSort(V, edges);
+
+            if (check(adj, V, res) == true)
+                System.out.println("true");
+            else
+                System.out.println("false");
+            System.out.println("~");
+        }
+    }
+
+    static boolean check(ArrayList<ArrayList<Integer>> adj, int V,
+                         ArrayList<Integer> res) {
+
+        if (V != res.size()) return false;
+
+        int[] map = new int[V];
+        for (int i = 0; i < V; i++) {
+            map[res.get(i)] = i;
+        }
+        for (int i = 0; i < V; i++) {
+            for (int v : adj.get(i)) {
+                if (map[i] > map[v]) return false;
+            }
+        }
+        return true;
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> topoSort(int V, int[][] edges) {
+        // code here
+           ArrayList<ArrayList<Integer>>adjList=new ArrayList<>();
+        for (int i=0;i<V;i++){
+            adjList.add(new ArrayList<>());
+        }
+        for (int[] edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            adjList.get(u).add(v);
+        }
+        int [] visited=new int [V];
+        Stack<Integer>stack=new Stack<>();
+        
+        for(int i=0;i<V;i++){
+            if (visited[i]==0){
+                dfs(i,visited,stack,adjList);
+            }
+        }
+        ArrayList<Integer>res=new ArrayList<>();
+        while(!stack.isEmpty()){
+            res.add(stack.pop());
+        }
+        return res;
+    }
+    private static void dfs(int ver,int [] visited,Stack<Integer>stack,ArrayList<ArrayList<Integer>>adjList){
+        visited[ver]=1;
+        for (int neighbour:adjList.get(ver)){
+            if (visited[neighbour]==0){
+                dfs(neighbour,visited,stack,adjList);
+            }
+        }
+        stack.push(ver);
+    }
+}
