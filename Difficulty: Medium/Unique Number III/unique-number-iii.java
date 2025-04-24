3e03977cae8d7e40a14eb70d4b1c64eeb31ee265
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-04-24 22:37:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Number III/unique-number-iii.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int ans = ob.getSingle(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int getSingle(int[] arr) {
+        // code here
+       int result = 0, x, sum;
+        for (int i = 0; i < 32; i++) {
+            sum = 0;
+            x = (1 << i);  
+            for (int j = 0; j < arr.length; j++) {
+                if ((arr[j] & x) != 0) {
+                    sum++;
+                }
+            }
+            if ((sum % 3) != 0) {
+                result |= x;
+            }
+        }
+
+        return result;
+    }
+}
