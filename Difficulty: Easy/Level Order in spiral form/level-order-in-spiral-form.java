80264543869243dc0e202e4b52761cd4423ba8aa
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-05-18 22:44:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Level Order in spiral form/level-order-in-spiral-form.java
@@ -0,0 +1,153 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+
+        inOrder(node.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution g = new Solution();
+            ArrayList<Integer> result = g.findSpiral(root);
+            for (int value : result) System.out.print(value + " ");
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    public ArrayList<Integer> findSpiral(Node root) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Deque<Node> dq = new ArrayDeque<>();
+        dq.add(root);
+        boolean reverse = true;
+        while (!dq.isEmpty()) {
+            int n = dq.size();
+            while (n-- > 0) {
+                if (reverse) {
+                    Node curr = dq.removeLast();
+                    res.add(curr.data);
+                    if (curr.right != null) dq.addFirst(curr.right);
+                    if (curr.left != null) dq.addFirst(curr.left);
+                } 
+                else {
+                    Node curr = dq.removeFirst();
+                    res.add(curr.data);
+                    if (curr.left != null) dq.addLast(curr.left);
+                    if (curr.right != null) dq.addLast(curr.right);
+                }
+            }
+            reverse = !reverse;
+        }
+
+        return res;
+    }
+}
