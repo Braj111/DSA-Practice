2f50481ba3e65e7b6beef92d7e18eb60e54899fd
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-05-20 22:51:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Burning Tree/burning-tree.java
@@ -0,0 +1,50 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}  */
+class Solution {
+     static int findDepth(Node root) {
+        if (root == null) return 0;
+    
+        int left = findDepth(root.left);
+        int right = findDepth(root.right);
+    
+        return Math.max(left, right) + 1;
+    }
+    static int findTarNode(Node root, int target, int[] ans) {
+        if (root == null) return -1;
+        if (root.data == target) {
+            int depth = findDepth(root) - 1;
+            ans[0] = Math.max(ans[0], depth);
+            return 1;
+        }
+    
+        int left = findTarNode(root.left, target, ans);
+        if (left != -1) {
+            int depth = findDepth(root.right);
+            ans[0] = Math.max(ans[0], left + depth);
+            return left + 1;
+        }
+        int right = findTarNode(root.right, target, ans);
+        if (right != -1) {
+            int depth = findDepth(root.left);
+            ans[0] = Math.max(ans[0], right + depth);
+            return right + 1;
+        }
+        return -1;
+    }
+    
+    static int minTime(Node root, int target) {
+        int[] ans = {0};
+        findTarNode(root, target, ans);
+        return ans[0];
+    }
+}
