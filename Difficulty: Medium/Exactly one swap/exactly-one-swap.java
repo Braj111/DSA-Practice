ed5e79a34754f383594f0672fe9a1f4982ae097c
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-06-10 11:43:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Exactly one swap/exactly-one-swap.java
@@ -0,0 +1,20 @@
+class Solution {
+    int countStrings(String s) {
+        // code here
+        int n = s.length();
+        int[] map = new int[26];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += (i - map[s.charAt(i) - 'a']);
+            map[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (map[i] > 1) {
+                ans++; 
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
