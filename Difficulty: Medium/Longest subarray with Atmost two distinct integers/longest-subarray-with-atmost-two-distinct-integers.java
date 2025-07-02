58dc6247f3e8c9d585f1b2dae34a0c778c314f71
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-07-02 22:04:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int totalElements(int[] arr) {
+        // code here
+         Map<Integer, Integer> mp = new HashMap<>();
+        int i = 0, j = 0, n = arr.length;
+        int size = 0;
+
+        while (j < n) {
+            mp.put(arr[j], mp.getOrDefault(arr[j], 0) + 1);
+            while (mp.size() > 2) {
+                mp.put(arr[i], mp.get(arr[i]) - 1);
+                if (mp.get(arr[i]) == 0)
+                    mp.remove(arr[i]);
+                i++; 
+            }
+
+            size = Math.max(size, j - i + 1);
+            j++; 
+        }
+
+        return size;
+    }
+}
