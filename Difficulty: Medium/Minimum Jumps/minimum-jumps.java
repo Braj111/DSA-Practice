74adcf614b30a783b0eb2f464316352e849854b1
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-09-11 20:51:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.java
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -1,58 +1,29 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            System.out.println(new Solution().minJumps(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    static int minJumps(int[] arr) {
+    public int minJumps(int[] arr) {
         // code here
         int n = arr.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[n - 1] = 0;
+        if (arr[0] == 0)
+            return -1;
+        int maxReach = 0;
+        int currReach = 0;
+        int jump = 0;
+        for (int i = 0; i < n; i++) {
+            maxReach = Math.max(maxReach, i + arr[i]);
 
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = i + 1; j <= i + arr[i] && j < n; j++) {
-                if (dp[j] != Integer.MAX_VALUE) {
-                    dp[i] = Math.min(dp[i], 1 + dp[j]);
+            if (maxReach >= n - 1) {
+                return jump + 1;
+            }
+            if (i == currReach) {
+                if (i == maxReach) {
+                    return -1;
+                }
+                else {
+                    jump++;
+                    currReach = maxReach;
                 }
             }
         }
-        if (dp[0] == Integer.MAX_VALUE)
-            return -1;
 
-        return dp[0];
+        return -1;
     }
 }
