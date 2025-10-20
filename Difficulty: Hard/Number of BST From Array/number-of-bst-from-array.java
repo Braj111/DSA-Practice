4e06a59df927623772fd56502a076ec22668b589
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-10-20 22:05:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Number of BST From Array/number-of-bst-from-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ArrayList<Integer> computeFact(int num) {
+        ArrayList<Integer> fact = new ArrayList<>(Collections.nCopies(num + 1, 1));
+        for (int i = 1; i <= num; i++)
+            fact.set(i, fact.get(i - 1) * i);
+        return fact;
+    }
+    public int catalan(int n, ArrayList<Integer> fact) {
+        return fact.get(2 * n) / (fact.get(n) * fact.get(n + 1));
+    }
+    public ArrayList<Integer> countBSTs(int[] arr) {
+        int n = arr.length;
+        int[][] sorted = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            sorted[i][0] = arr[i];
+            sorted[i][1] = i;
+        }
+        Arrays.sort(sorted, Comparator.comparingInt(a -> a[0]));
+
+        ArrayList<Integer> fact = computeFact(2 * n);
+
+        ArrayList<Integer> numBSTs = new ArrayList<>(Collections.nCopies(n, 0));
+        for (int i = 0; i < n; i++) {
+            int j = sorted[i][1];
+            numBSTs.set(j, catalan(i, fact) * catalan(n - i - 1, fact));
+        }
+
+        return numBSTs;
+    }
+}
