53a1270c1c60832de1c2d06eeae635e0e4a11406
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2025-11-21 21:00:35 +0530

    Time: 271 ms (13.98%), Space: 46.7 MB (30.09%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int countPalindromicSubsequence(String s) {
-        int[] first= new int[26];
-        int[] last= new int[26];
+  public int countPalindromicSubsequence(String s) {
+    int ans = 0;
+    int[] first = new int[26];
+    int[] last = new int[26];
 
-        Arrays.fill(first, s.length());
+    Arrays.fill(first, s.length());
+    Arrays.fill(last, -1);
 
-        for(int i=0; i<s.length(); i++){
-            int pos= s.charAt(i)-'a';
-            first[pos]=Math.min(first[pos], i);
-            last[pos]=i;
-        }
-        int res=0;
-        for(int i=0; i<26; i++){
-            if(first[i]<last[i]){
-                res+=s.substring(first[i]+1,last[i]).chars().distinct().count();
-            }
-            
-        } 
-        return res;  
+    for (int i = 0; i < s.length(); ++i) {
+      final int index = s.charAt(i) - 'a';
+      first[index] = Math.min(first[index], i);
+      last[index] = i;
     }
+
+    for (int i = 0; i < 26; ++i)
+      if (first[i] < last[i])
+        ans += s.substring(first[i] + 1, last[i]).chars().distinct().count();
+
+    return ans;
+  }
 }
