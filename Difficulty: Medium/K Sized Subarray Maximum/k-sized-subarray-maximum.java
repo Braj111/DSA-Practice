77f82a7df0a267ebf93c5dc42af091922346c5d6
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2026-02-01 22:13:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -1,86 +1,31 @@
-//{ Driver Code Starts
-//Initial template for JAVA
+class Solution {
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        int n = arr.length;
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        Deque<Integer> dq = new ArrayDeque<Integer>();
 
-import java.util.*;
-import java.io.*;
-import java.lang.*;
-
-class Main
-{
-    public static void main(String args[])
-    {
-        //taking input using class Scanner
-        Scanner sc = new Scanner(System.in);
-        
-        //taking total count of testcases
-        int t = sc.nextInt();
-        
-        
-        
-        while(t-- > 0)
-        {
-            //taking total number of elements
-            int n = sc.nextInt();
-            
-            //taking size of subArrays 
-            int k = sc.nextInt();
-            
-            //Declaring and Intializing an array of size n
-            int arr[] = new int[n];
-            
-            //adding all the elements to the array 
-            for(int i = 0; i <n; i++)
-            {
-                arr[i] =sc.nextInt();
+        for (int i = 0; i < k; ++i) {
+            while (!dq.isEmpty() && arr[i] >= arr[dq.peekLast()]) {
+                dq.pollLast();
             }
-            
-            //Calling the method max_of_subarrays of class solve
-            //and storing the result in an ArrayList
-            ArrayList <Integer> res = new Solution().max_of_subarrays(arr, n, k);
-            
-            //printing the elements of the ArrayList
-            for (int i = 0; i < res.size(); i++)
-                System.out.print (res.get (i) + " ");
-            System.out.println();
+            dq.addLast(i);
         }
-    }
-}
-// } Driver Code Ends
 
+        for (int i = k; i < arr.length; ++i) {
+            res.add(arr[dq.peekFirst()]);
 
-//User function template for JAVA
-
-class Solution
-{
-    //Function to find maximum of each subarray of size k.
-    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
-    {
-        // Your code here
-        ArrayList <Integer> ans=new ArrayList <Integer>();
-        Deque<Integer> d= new LinkedList<Integer>();
-        int i;
-        for(i=0; i<k; i++){
-            while(!d.isEmpty() && arr[i]>=arr[d.peekLast()]){
-                d.removeLast();
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
             }
-            d.addLast(i);
+
+            while (!dq.isEmpty() && arr[i] >= arr[dq.peekLast()]) {
+                dq.pollLast();
+            }
+
+            dq.addLast(i);
         }
-        
-        for(;i<n;i++){
-            ans.add(arr[d.peek()]);
-            
-            while(!d.isEmpty() && d.peek() <=i-k){
-                d.removeFirst();
-            }
-            
-            while(!d.isEmpty() && arr[i]>=arr[d.peekLast()]){
-                d.removeLast();
-            }
-            
-            d.addLast(i);
-        }
-        ans.add(arr[d.peek()]);
-        
-        return ans;
+
+        res.add(arr[dq.peekFirst()]);
+        return res;
     }
 }
