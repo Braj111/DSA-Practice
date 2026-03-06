187cee6716a7c4ec3fad06be96ff4d9a440c570c
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2026-03-06 22:33:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
@@ -1,43 +1,45 @@
 class Solution {
-   public static String smallestWindow(String s, String p) {
-    int len1 = s.length();
-    int len2 = p.length();
+    public static String minWindow(String s, String p) {
+        // code here
+        int len1 = s.length();
+        int len2 = p.length();
 
-    if (len1 < len2)
-        return "";
+        if (len1 < len2)
+            return "";
 
-    int[] countP = new int[256];
-    int[] countS = new int[256];
-    for (int i = 0; i < len2; i++)
-        countP[p.charAt(i)]++;
+        int[] countP = new int[256];
+        int[] countS = new int[256];
+        for (int i = 0; i < len2; i++)
+            countP[p.charAt(i)]++;
 
-    int start = 0, start_idx = -1, min_len = Integer.MAX_VALUE;
-    int count = 0;
+        int start = 0, start_idx = -1, min_len = Integer.MAX_VALUE;
+        int count = 0;
 
-    for (int j = 0; j < len1; j++) {
-        char currChar = s.charAt(j);
-        countS[currChar]++;
-        if (countP[currChar] != 0 && countS[currChar] <= countP[currChar])
-            count++;
-        if (count == len2) {
-            while (countS[s.charAt(start)] > countP[s.charAt(start)] || countP[s.charAt(start)] == 0) {
-                if (countS[s.charAt(start)] > countP[s.charAt(start)])
-                    countS[s.charAt(start)]--;
-                start++;
+        for (int j = 0; j < len1; j++) {
+            char currChar = s.charAt(j);
+            countS[currChar]++;
+            if (countP[currChar] > 0 && countS[currChar] <= countP[currChar]) {
+                count++;
             }
-
-            int windowLen = j - start + 1;
-            if (min_len > windowLen) {
-                min_len = windowLen;
-                start_idx = start;
+            if (count == len2) {
+                char startChar;
+                while (countS[startChar = s.charAt(start)] > countP[startChar] || countP[startChar] == 0) {
+                    if (countS[startChar] > countP[startChar]) {
+                        countS[startChar]--;
+                    }
+                    start++;
+                }
+                int len = j - start + 1;
+                if (min_len > len) {
+                    min_len = len;
+                    start_idx = start;
+                }
             }
         }
+
+        if (start_idx == -1)
+            return "";
+
+        return s.substring(start_idx, start_idx + min_len);
     }
-
-    if (start_idx == -1)
-        return "";
-
-    return s.substring(start_idx, start_idx + min_len);
-}
-
 }
