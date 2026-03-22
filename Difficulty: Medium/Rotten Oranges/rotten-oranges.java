e2196d2933d72dc7a9211c69143505c4bde58b07
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2026-03-22 22:03:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotten Oranges/rotten-oranges.java
+++ Difficulty: Medium/Rotten Oranges/rotten-oranges.java
@@ -1,78 +1,50 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-
-            int mat[][] = new int[n][m];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) mat[i][j] = sc.nextInt();
-            }
-            Solution obj = new Solution();
-            int ans = obj.orangesRotting(mat);
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    static boolean isSafe(int i, int j, int n, int m)
-    {
+   public boolean isSafe(int i, int j, int n, int m) {
         return (i >= 0 && i < n && j >= 0 && j < m);
     }
-
-    static int orangesRotting(int[][] mat)
-    {
+    public int orangesRot(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        int[][] directions= { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
         Queue<int[]> q = new LinkedList<>();
+        int elapsedTime = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 2) {
-                    q.add(new int[] { i, j });
+                    q.add(new int[]{i, j});
                 }
             }
         }
-        int elapsedTime = 0;
-
+        int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
         while (!q.isEmpty()) {
-            elapsedTime++;
-            int len = q.size();
-            while (len-- > 0) {
-                int[] cur = q.poll();
-                int i = cur[0];
-                int j = cur[1];
+            int size = q.size();
+            boolean flag = false; 
+            for (int i = 0; i < size; i++) {
+                int[] cell = q.poll();
+                int x = cell[0];
+                int y = cell[1];
                 for (int[] dir : directions) {
-                    int x = i + dir[0];
-                    int y = j + dir[1];
-                    if (isSafe(x, y, n, m)
-                        && mat[x][y] == 1) {
-                        mat[x][y] = 2;
-                        q.add(new int[] { x, y });
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];
+                    if (isSafe(nx, ny, n, m) && mat[nx][ny] == 1) {
+                        mat[nx][ny] = 2;
+                        q.add(new int[]{nx, ny});
+                        flag = true;
                     }
                 }
             }
+
+            if (flag)
+                elapsedTime++;
         }
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 1) {
+                if (mat[i][j] == 1)
                     return -1;
-                }
             }
         }
 
-        return Math.max(0, elapsedTime - 1);
+        return elapsedTime;
     }
+
 }
