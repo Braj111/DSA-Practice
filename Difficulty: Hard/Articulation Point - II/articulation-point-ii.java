a1902120ec1700287b223baa8728bce2595c4854
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2026-03-28 22:38:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Articulation Point - II/articulation-point-ii.java
+++ Difficulty: Hard/Articulation Point - II/articulation-point-ii.java
@@ -1,83 +1,65 @@
-//{ Driver Code Starts
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        while (tc-- > 0) {
-            int V = sc.nextInt();
-            int E = sc.nextInt();
-            int[][] edges = new int[E][2];
-            for (int i = 0; i < E; i++) {
-                edges[i][0] = sc.nextInt();
-                edges[i][1] = sc.nextInt();
-            }
-
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.articulationPoints(V, edges);
-            Collections.sort(ans);
-            for (int val : ans) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    static ArrayList<ArrayList<Integer>> constructAdj(int V, int[][] edges) {
+    static int timer;
+    
+    static ArrayList<Integer> articulationPoints(int V, int[][] edges) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
-        
-        for (int[] edge : edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
-        return adj;
+        
+        boolean[] visited = new boolean[V];
+        int[] tin = new int[V];  
+        int[] low = new int[V];   
+        boolean[] isArticulation = new boolean[V];
+        
+        timer = 0;
+        
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfs(i, -1, visited, tin, low, adj, isArticulation);
+            }
+        }
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            if (isArticulation[i]) result.add(i);
+        }
+        
+        if (result.size() == 0) {
+            result.add(-1);
+        }
+        
+        return result;
     }
-     static void findPoints(ArrayList<ArrayList<Integer>> adj, int u, int[] visited, int[] disc, int[] low, int[] time, int parent, int[] isAP) {
-        visited[u] = 1;
-        disc[u] = low[u] = ++time[0];
-        int children = 0; 
-        for (int v : adj.get(u)) {
-            if (visited[v] == 0) {
-                children++;
-                findPoints(adj, v, visited, disc, low, time, u, isAP);
-                low[u] = Math.min(low[u], low[v]);
-                if (parent != -1 && low[v] >= disc[u]) {
-                    isAP[u] = 1;
+    
+    static void dfs(int node, int parent, boolean[] visited, int[] tin, int[] low,
+                    ArrayList<ArrayList<Integer>> adj, boolean[] isArticulation) {
+        
+        visited[node] = true;
+        tin[node] = low[node] = timer++;
+        int children = 0;
+        
+        for (int neighbor : adj.get(node)) {
+            if (neighbor == parent) continue;
+            
+            if (!visited[neighbor]) {
+                dfs(neighbor, node, visited, tin, low, adj, isArticulation);
+                
+                low[node] = Math.min(low[node], low[neighbor]);
+                if (low[neighbor] >= tin[node] && parent != -1) {
+                    isArticulation[node] = true;
                 }
-            } 
-            else if (v != parent) {
-                low[u] = Math.min(low[u], disc[v]);
+                
+                children++;
+            } else {
+                low[node] = Math.min(low[node], tin[neighbor]);
             }
         }
         if (parent == -1 && children > 1) {
-            isAP[u] = 1;
+            isArticulation[node] = true;
         }
-    }
-     static ArrayList<Integer> articulationPoints(int V, int[][] edges) {
-        
-        ArrayList<ArrayList<Integer>> adj = constructAdj(V, edges);
-        int[] disc = new int[V], low = new int[V],
-        visited = new int[V], isAP = new int[V];
-        int[] time = {0}; 
-        for (int u = 0; u < V; u++) {
-            if (visited[u] == 0) {
-                findPoints(adj, u, visited, disc, low, time, -1, isAP);
-            }
-        }
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int u = 0; u < V; u++) {
-            if (isAP[u] == 1) {
-                result.add(u);
-            }
-        }
-        if (result.isEmpty()) result.add(-1);
-        return result;
     }
 }
