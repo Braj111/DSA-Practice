2578e1de2b438a2b1f31e27baae3fd0e4643d37b
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2026-04-05 19:57:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Target Sum/target-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int totalWays(int[] arr, int target) {
+        int n = arr.length;
+        int totalSum = 0;
+
+        for (int x : arr)
+            totalSum += x;
+
+        int size = 2 * totalSum + 1;
+        int[] prev = new int[size];
+        int[] curr = new int[size];
+
+        prev[totalSum] = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            Arrays.fill(curr, 0);
+            int val = arr[i - 1];
+
+            for (int sum = 0; sum < size; sum++) {
+                if (sum + val < size)
+                    curr[sum + val] += prev[sum];
+                if (sum - val >= 0)
+                    curr[sum - val] += prev[sum];
+            }
+
+            prev = curr.clone();
+        }
+
+        return prev[target + totalSum];
+    }
+}
