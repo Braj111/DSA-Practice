84a1bad73a80c80872fe586e6cd34154b5927e78
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2026-04-15 22:00:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/URLify a given string/urlify-a-given-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    String URLify(String s) {
+        // code here
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) ==' ') {
+                result.append("%");
+                result.append("2");
+                result.append("0");
+            }
+            else {
+                result.append(s.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+}
