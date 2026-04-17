27475efb043f03a40ca5266b26ad00d32bd90c45
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2026-04-17 22:56:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Anagram Palindrome/anagram-palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    boolean canFormPalindrome(String s) {
+        // code here
+        int mask = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int bit = s.charAt(i) - 'a';
+            mask ^= (1 << bit);
+        }
+        return (mask == 0) || ((mask & (mask - 1)) == 0);
+    }
+}
