07b0f3a5ea213a8dedd40aa27ec50d3306412ea5
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2026-05-01 23:04:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Largest in a Stream/kth-largest-in-a-stream.java
@@ -0,0 +1,25 @@
+class Solution {
+    static ArrayList<Integer> kthLargest(int[] arr, int k) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        PriorityQueue<Integer> minHeap
+            = new PriorityQueue<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (minHeap.size() < k) {
+                minHeap.add(arr[i]);
+            }
+            else if (arr[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.add(arr[i]);
+            }
+            if (minHeap.size() == k) {
+                res.add(minHeap.peek());
+            }
+            else {
+                res.add(-1);
+            }
+        }
+        return res;
+    }
+}
