663425e7389abcf73695e6ec07559c4aee24beb1
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2026-07-07 23:08:10 +0530

    Time: 1 ms (99.85%), Space: 42.6 MB (67.71%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long sumAndMultiply(int n) {
+        long x = 0;
+        long sum = 0;
+        String s = Integer.toString(n);
+        
+        for (char ch : s.toCharArray()) {
+            if (ch != '0') {
+                int digit = ch - '0';
+                x = x * 10 + digit;
+                sum += digit;
+            }
+        }
+        return x * sum;
+    }
+}
