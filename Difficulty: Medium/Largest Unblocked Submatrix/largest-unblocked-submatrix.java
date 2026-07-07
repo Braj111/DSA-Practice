b5e85ab8fa3a28f0560d40e1bebf83bd6bb326b4
BRAJ KISHOR SHARMA <92883537+Braj111@users.noreply.github.com>
2026-07-07 23:08:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest Unblocked Submatrix/largest-unblocked-submatrix.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int largestArea(int n, int m, int[][] arr) {
+        // code here
+        int k = arr.length;
+        List<Integer> rows = new ArrayList<>();
+        List<Integer> cols = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            rows.add(arr[i][0]);
+            cols.add(arr[i][1]);
+        }
+        rows.add(0);
+        rows.add(n + 1);
+
+        cols.add(0);
+        cols.add(m + 1);
+        Collections.sort(rows);
+        Collections.sort(cols);
+
+        int maxRowGap = 0;
+        int maxColGap = 0;
+        for (int i = 1; i < rows.size(); i++) {
+            maxRowGap = Math.max(maxRowGap,
+                                 rows.get(i)
+                                     - rows.get(i - 1) - 1);
+        }
+        for (int i = 1; i < cols.size(); i++) {
+            maxColGap = Math.max(maxColGap,
+                                 cols.get(i)
+                                     - cols.get(i - 1) - 1);
+        }
+        return maxRowGap * maxColGap;
+    }
+}
